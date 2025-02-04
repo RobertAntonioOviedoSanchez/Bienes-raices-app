567da18a325d3691e0ed16e5afc1a0d66b0d0ac2
RobertAntonioOviedoSanchez <robertoviedo35@gmail.com>
2025-02-04 10:48:07 -0400

    Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,35 @@
 "use client"
+import Link from "next/link"
+import { CiMenuFries } from "react-icons/ci"
+import { BsTelephone } from "react-icons/bs"
+import { Navbar } from "../Navbar"
+import { useState } from "react"
 
 export function Header() {
 
+    const [ openMobileMenu, setOpenMobileMenu ] = useState(false)
+
     return (
-        <p>Header</p>
+        <div className="container mx-auto my-5">
+            <div className="flex items-center justify-between px-5 md:px-0">
+                <Link href={"/"}>
+                    <h1 className="font-medium">BienesRaices</h1>
+                </Link>
+
+                <CiMenuFries className="block text-2xl md:hidden" onClick={() => setOpenMobileMenu(!openMobileMenu)} />
+
+                <Navbar openMobileMenu={openMobileMenu} />
+
+                <div  className="flex items-center gap-2 md:gap-5">
+                    <Link href="tel:0123456789" className="flex items-center gap-4 cursor-pointer">
+                        <BsTelephone />
+                        <span className="hidden md:block">+58 4241234567</span>
+                    </Link>
+                    <Link href={"/login"} className="px-3 py-2 text-white rounded-lg bg-secondary hover:bg-black">
+                        Login
+                    </Link>
+                </div>
+            </div>
+        </div>
     )
 }
