325f47614738be3b063f9d1a17bcf609eecb6ce3
RobertAntonioOviedoSanchez <robertoviedo35@gmail.com>
2025-02-09 15:01:07 -0400

    error 404

--- src/app/properties/(property)/[page]/page.tsx
+++ src/app/properties/(property)/[page]/page.tsx
@@ -1,10 +1,32 @@
 'use client'
 
+import { Error404 } from "@/components/Error404"
+import { Footer } from "@/components/Footer"
+import { Header } from "@/components/Header"
+import { dataProperties } from "@/components/Properties/Properties.data"
+import { Property } from "@/components/Property"
+import { Transition } from "@/components/Transition"
+import { TransitionPage } from "@/components/TransitionPage"
+import { useParams } from "next/navigation"
+
 export default function Page() {
+
+    const router = useParams()
+    const propertyId = router.page
+    const filteredPage = dataProperties.find(property => property.id === Number(propertyId))
 
     return (
         <>
-            <p>Page</p>
+            <TransitionPage />
+            <Header />
+            {filteredPage ? (
+                <Property />
+            )
+            : (
+                <Error404 />
+            )}
+
+            <Footer />
         </>
     )
 }
