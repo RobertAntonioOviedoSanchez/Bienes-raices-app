bbbf5712b4b53e366980be9fd4c0a5b8b628f364
RobertAntonioOviedoSanchez <robertoviedo35@gmail.com>
2025-02-12 16:03:18 -0400

    Form

--- src/components/Property/Property.tsx
+++ src/components/Property/Property.tsx
@@ -12,7 +12,7 @@ export function Property(props: PropertyProps) {
 
     return (
         <main className="max-w-5xl mx-auto">
-            <div className="grid md:grid-cols-[70%, 1fr] my-3 py-5">
+            <div className="grid md:grid-cols-[70%,1fr] my-3 py-5">
                 <div className="px-6">
                     <h1 className="text-3xl mb-4 text-secondary flex justify-between">
                         <span>House {house.id}</span>
@@ -47,8 +47,8 @@ export function Property(props: PropertyProps) {
                     <div className="my-3">{house.description}</div>
                 </div>
 
+                <Form />
             </div>
-            <Form />
         </main>
     )
 }
